ccf6495879123ae4cf0facab0bfce4478fbf2297
Ben Zuba <zubes33@gmail.com>
2016-11-29 20:58:40 +0000

    allow tasks creation

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -5,26 +5,30 @@ RSpec.describe TasksController, type: :controller do
     it "should list the tasks in the database" do
       task1 = FactoryGirl.create(:task)
       task2 = FactoryGirl.create(:task)
-      task1.update_attributes(title: "Something else")
       get :index
-      expect(response).to have_http_status :success
+      assert_response :success
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(2)
-      response_ids = response_value.collect do |task|
-        task["id"]
-
-      end
-      expect(response_ids).to eq([task1.id, task2.id])
     end
   end
 
-  describe "task#update" do
-    it "should allow tasks to be marked as done" do 
+  describe "tasks#update" do
+    it "should allow tasks to be marked as done" do
       task = FactoryGirl.create(:task, done: false)
       put :update, id: task.id, task: { done: true }
       expect(response).to have_http_status(:success)
       task.reload
       expect(task.done).to eq(true)
+    end
+  end
+
+  describe "task#create" do
+    it "should allow new tasks to be created" do
+      post :create, task: {title: "Fix things"}
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq("Fix things")
+      expect(Task.last.title).to eq("Fix things")
     end
   end
 end
